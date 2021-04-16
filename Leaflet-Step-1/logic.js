b34cee734e5a13996bf2531bd4e4fdf442b261fa
Chelsea Ball <chelseaball@Chelseas-MacBook-Air.local>
2021-04-16 15:51:48 -0700

    Created legend

--- Leaflet-Step-1/logic.js
+++ Leaflet-Step-1/logic.js
@@ -1,6 +1,5 @@
 // Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
-  "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 
 // Perform a GET request to the query URL
 d3.json(queryUrl).then(function(data) {
@@ -54,8 +53,31 @@ d3.json(queryUrl).then(function(data) {
       }
   }).addTo(earthquakes)
   
-  
   earthquakes.addTo(myMap)
+
+  let legend = L.control({
+      position: "bottomright"
+  })
+
+  legend.onAdd = function(){
+      var div = L.DomUtil.create('div', 'info legend');
+      const magnitudes = [0, 1, 2, 3, 4, 5]
+      const colors = [
+          "purple",
+          "blue",
+          "green",
+          "yellow",
+          "orange",
+          "red"
+      ]
+
+      for(var i = 0; i < magnitudes.length; i++){
+          div.innerHTML += `<i style='background:${colors[i]}'> </i>` + magnitudes[i] + (magnitudes[i+1] ? "&ndash;" + magnitudes[i+1] + "<br>": "+")
+      }
+      return div;
+  }
+
+  legend.addTo(myMap)
 });
 
   // Define streetmap and darkmap layers
